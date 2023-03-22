4292e54f7c518c0fdf48bf5efc2d120f668856f8
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-03-22 11:04:12 +0100

    set temppath

--- src/Middleware/session/temp.php
+++ src/Middleware/session/temp.php
@@ -4,12 +4,11 @@ use Tualo\Office\Basic\TualoApplication;
 TualoApplication::use('TualoApplicationSessionInitTemp',function(){
     try{
         if (isset($_SESSION['tualoapplication']['loggedIn'])&&($_SESSION['tualoapplication']['loggedIn']===true)){
-            /*
             TualoApplication::set('sid',TualoApplication::get('session')->id());
             $path = TualoApplication::get('basePath').'/temp/'.TualoApplication::get('sid');
             TualoApplication::set('tempPath',$path);
             if(!file_exists($path)){ mkdir($path,0777,true); }
-            */
+            
         }
     }catch(\Exception $e){
         TualoApplication::set('maintanceMode','on');
