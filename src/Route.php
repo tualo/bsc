df0c4dcb4d850328354f3a40fac753ace05540ce
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-04-12 13:29:49 +0200

    test options

--- src/Route.php
+++ src/Route.php
@@ -43,6 +43,14 @@ class Route{
         }
         $path = preg_replace('/\/\//','/',$path);
         $method = $_SERVER['REQUEST_METHOD'];
+        if ($method=='OPTIONS'){
+            header("HTTP/1.0 200 OK");
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+            header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+            header('Access-Control-Max-Age: 86400');
+            exit();
+        }
         self::runpath($path,$method);
     }
 
