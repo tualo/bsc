d0679425cd6a1da88026fe6b3ae030bf501fa34e
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-08-06 09:50:42 +0200

    add create_index proc

--- src/sql/install/ds/proc.create__fulltext_index.sql
+++ src/sql/install/ds/proc.create__fulltext_index.sql
@@ -26,3 +26,33 @@ IF IndexIsThere = 0 THEN
 END IF;
 
 END //
+
+
+CREATE OR REPLACE PROCEDURE `create_index`(
+    given_database VARCHAR(64),
+    given_table    VARCHAR(64),
+    given_index    VARCHAR(64),
+    given_columns  VARCHAR(64)
+)
+BEGIN
+
+    DECLARE IndexIsThere INTEGER;
+
+    SELECT COUNT(1) INTO IndexIsThere
+    FROM INFORMATION_SCHEMA.STATISTICS
+    WHERE table_schema = given_database
+    AND   table_name   = given_table
+    AND   index_name   = given_index;
+
+    IF IndexIsThere = 0 THEN
+        SET @sqlstmt = CONCAT('CREATE INDEX ',given_index,' ON ',
+        given_database,'.',given_table,' (',given_columns,')');
+        PREPARE st FROM @sqlstmt;
+        EXECUTE st;
+        DEALLOCATE PREPARE st;
+    ELSE
+        SELECT CONCAT('Index ',given_index,' already exists on Table ',
+        given_database,'.',given_table) CreateindexErrorMessage;
+    END IF;
+
+END //
