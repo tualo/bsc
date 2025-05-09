119a17d2590ba442a4fed7ac5749d9e54f367bbf
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-05-09 09:48:15 +0200

    fix install

--- src/CreateSystemCommandline.php
+++ src/CreateSystemCommandline.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Tualo\Office\Basic;
+
 use Garden\Cli\Cli;
 use Garden\Cli\Args;
 use Tualo\Office\Basic\ICommandline;
@@ -10,11 +12,16 @@ use Tualo\Office\DS\DataRenderer;
 use Ramsey\Uuid\Uuid;
 
 
-class CreateSystemCommandline implements ICommandline{
+class CreateSystemCommandline implements ICommandline
+{
 
-    public static function getCommandName():string { return 'createsystem';}
+    public static function getCommandName(): string
+    {
+        return 'createsystem';
+    }
 
-    public static function setup(Cli $cli){
+    public static function setup(Cli $cli)
+    {
         $cli->command(self::getCommandName())
             ->description('create a new system')
 
@@ -27,22 +34,23 @@ class CreateSystemCommandline implements ICommandline{
             ->opt('session', 'session db name', false, 'string');
     }
 
-    
 
-    public static function run(Args $args){
+
+    public static function run(Args $args)
+    {
         $prompt = [
-            "\t".'do you want to create a new system? [y|n|c] '
+            "\t" . 'do you want to create a new system? [y|n|c] '
         ];
-        $line =( $args->getOpt('silent',false))?'y':'';
-        
-        while( ($line == 'y') || in_array($line = readline(implode("\n",$prompt)),['yes','y','n','no','c']) ){
-            if ($line=='c') exit();
-            if ($line=='y'){
-                if (($clientDBName = $args->getOpt('db'))==''){
+        $line = ($args->getOpt('silent', false)) ? 'y' : '';
+
+        while (($line == 'y') || in_array($line = readline(implode("\n", $prompt)), ['yes', 'y', 'n', 'no', 'c'])) {
+            if ($line == 'c') exit();
+            if ($line == 'y') {
+                if (($clientDBName = $args->getOpt('db')) == '') {
                     $clientDBName = readline("Enter the client db name: ");
                 }
-                if (($sessionDBName = $args->getOpt('session'))==''){
-                    if (($sessionDBName = App::configuration('','__SESSION_DSN__',''))==''){
+                if (($sessionDBName = $args->getOpt('session')) == '') {
+                    if (($sessionDBName = App::configuration('', '__SESSION_DSN__', '')) == '') {
                         $sessionDBName = readline("Enter the session db name: ");
                     }
                 }
@@ -55,77 +63,77 @@ class CreateSystemCommandline implements ICommandline{
                 $client_database_user = "";
 
                 $clientOptions = "";
-                if (($client_host = $args->getOpt('host'))!='') $clientOptions .= " --host=".$client_host." ";
-                if (($client_username = $args->getOpt('username'))!='') $clientOptions .= " --user=".$client_username." ";
-                if (($client_password = $args->getOpt('password'))!='') $clientOptions .= ' --password="'.$client_password.'" ';
-                
+                if (($client_host = $args->getOpt('host')) != '') $clientOptions .= " --host=" . $client_host . " ";
+                if (($client_username = $args->getOpt('username')) != '') $clientOptions .= " --user=" . $client_username . " ";
+                if (($client_password = $args->getOpt('password')) != '') $clientOptions .= ' --password="' . $client_password . '" ';
 
-                
 
-                if (( $args->getOpt('createusers'))===true){
-                    PostCheck::formatPrint(['blue'],"\tcreate database users ... ");
+
+
+                if (($args->getOpt('createusers')) === true) {
+                    PostCheck::formatPrint(['blue'], "\tcreate database users ... ");
                     $session_database_password = (Uuid::uuid4())->toString();
-                    $session_database_user = 'user_'.$sessionDBName;
+                    $session_database_user = 'user_' . $sessionDBName;
 
                     $client_database_password = (Uuid::uuid4())->toString();
-                    $client_database_user = 'user_'.$clientDBName;
+                    $client_database_user = 'user_' . $clientDBName;
 
 
-                    PostCheck::formatPrint(['blue'],"\tsession database user: ".$session_database_user." \n");
-                    PostCheck::formatPrint(['blue'],"\tsession database password: ".$session_database_password." \n");
+                    PostCheck::formatPrint(['blue'], "\tsession database user: " . $session_database_user . " \n");
+                    PostCheck::formatPrint(['blue'], "\tsession database password: " . $session_database_password . " \n");
 
 
-                    PostCheck::formatPrint(['blue'],"\tclient database user: ".$client_database_user." \n");
-                    PostCheck::formatPrint(['blue'],"\tclient database password: ".$client_database_password." \n");
+                    PostCheck::formatPrint(['blue'], "\tclient database user: " . $client_database_user . " \n");
+                    PostCheck::formatPrint(['blue'], "\tclient database password: " . $client_database_password . " \n");
 
-                    exec('mysql '.$clientOptions.' -e "create user if not exists \''.$session_database_user.'\'@\'%\' identified by \''.$session_database_password.'\';"',$res,$err);
-                    if ($err!=0){
-                        PostCheck::formatPrintLn(['red'],'failed');
-                        PostCheck::formatPrintLn(['red'],implode("\n",$res));
+                    exec('mysql ' . $clientOptions . ' -e "create user if not exists \'' . $session_database_user . '\'@\'%\' identified by \'' . $session_database_password . '\';"', $res, $err);
+                    if ($err != 0) {
+                        PostCheck::formatPrintLn(['red'], 'failed');
+                        PostCheck::formatPrintLn(['red'], implode("\n", $res));
                         //exit();
                     }
 
-                    exec('mysql '.$clientOptions.' -e "create user if not exists \''.$client_database_user.'\'@\'%\' identified by \''.$client_database_password.'\';"',$res,$err);
-                    if ($err!=0){
-                        PostCheck::formatPrintLn(['red'],'failed');
-                        PostCheck::formatPrintLn(['red'],implode("\n",$res));
+                    exec('mysql ' . $clientOptions . ' -e "create user if not exists \'' . $client_database_user . '\'@\'%\' identified by \'' . $client_database_password . '\';"', $res, $err);
+                    if ($err != 0) {
+                        PostCheck::formatPrintLn(['red'], 'failed');
+                        PostCheck::formatPrintLn(['red'], implode("\n", $res));
                         //exit();
                     }
 
-                    exec('mysql '.$clientOptions.' -e "grant all on '.$sessionDBName.'.* to \''.$session_database_user.'\'@\'%\';"',$res,$err);
-                    if ($err!=0){
-                        PostCheck::formatPrintLn(['red'],'failed');
-                        PostCheck::formatPrintLn(['red'],implode("\n",$res));
+                    exec('mysql ' . $clientOptions . ' -e "grant all on ' . $sessionDBName . '.* to \'' . $session_database_user . '\'@\'%\';"', $res, $err);
+                    if ($err != 0) {
+                        PostCheck::formatPrintLn(['red'], 'failed');
+                        PostCheck::formatPrintLn(['red'], implode("\n", $res));
                         //exit();
                     }
 
-                    exec('mysql '.$clientOptions.' -e "grant all on '.$clientDBName.'.* to \''.$client_database_user.'\'@\'%\';"',$res,$err);
-                    if ($err!=0){
-                        PostCheck::formatPrintLn(['red'],'failed');
-                        PostCheck::formatPrintLn(['red'],implode("\n",$res));
+                    exec('mysql ' . $clientOptions . ' -e "grant all on ' . $clientDBName . '.* to \'' . $client_database_user . '\'@\'%\';"', $res, $err);
+                    if ($err != 0) {
+                        PostCheck::formatPrintLn(['red'], 'failed');
+                        PostCheck::formatPrintLn(['red'], implode("\n", $res));
                         exit();
                     }
                 }
 
 
-                PostCheck::formatPrint(['blue'],"\tcreate database *".$sessionDBName."* using mysql client... ");
-                exec('mysql '.$clientOptions.' -e "create database if not exists '.$sessionDBName.';"',$res,$err);
-                if ($err!=0){
-                    PostCheck::formatPrintLn(['red'],'failed');
-                    PostCheck::formatPrintLn(['red'],implode("\n",$res));
+                PostCheck::formatPrint(['blue'], "\tcreate database *" . $sessionDBName . "* using mysql client... ");
+                exec('mysql ' . $clientOptions . ' -e "create database if not exists ' . $sessionDBName . ';"', $res, $err);
+                if ($err != 0) {
+                    PostCheck::formatPrintLn(['red'], 'failed');
+                    PostCheck::formatPrintLn(['red'], implode("\n", $res));
                     exit();
-                }else{
-                    PostCheck::formatPrintLn(['green'],'done');
+                } else {
+                    PostCheck::formatPrintLn(['green'], 'done');
                 }
 
-                PostCheck::formatPrint(['blue'],"\tcreate database ".$clientDBName." using mysql client... ");
-                exec('mysql '.$clientOptions.' -e "create database if not exists '.$clientDBName.';"',$res,$err);
-                if ($err!=0){
-                    PostCheck::formatPrintLn(['red'],'failed');
-                    PostCheck::formatPrintLn(['red'],implode("\n",$res));
+                PostCheck::formatPrint(['blue'], "\tcreate database " . $clientDBName . " using mysql client... ");
+                exec('mysql ' . $clientOptions . ' -e "create database if not exists ' . $clientDBName . ';"', $res, $err);
+                if ($err != 0) {
+                    PostCheck::formatPrintLn(['red'], 'failed');
+                    PostCheck::formatPrintLn(['red'], implode("\n", $res));
                     exit();
-                }else{
-                    PostCheck::formatPrintLn(['green'],'done');
+                } else {
+                    PostCheck::formatPrintLn(['green'], 'done');
                 }
 
                 /*
@@ -138,17 +146,17 @@ class CreateSystemCommandline implements ICommandline{
                     
                 }
                 */
-                PostCheck::formatPrint(['blue'],"\tsetup sessions db... ");
+                PostCheck::formatPrint(['blue'], "\tsetup sessions db... ");
 
-                exec('mysql '.$clientOptions.' --force=true -D '.$sessionDBName.' < '.__DIR__.'/commandline/sql/plain-system-session.sql',$res,$err);
-                if ($err!=0){
-                    PostCheck::formatPrintLn(['red'],'failed');
-                    PostCheck::formatPrintLn(['red'],implode("\n",$res));
+                exec('mysql ' . $clientOptions . ' --force=true -D ' . $sessionDBName . ' < ' . __DIR__ . '/commandline/sql/plain-system-session.sql', $res, $err);
+                if ($err != 0) {
+                    PostCheck::formatPrintLn(['red'], 'failed');
+                    PostCheck::formatPrintLn(['red'], implode("\n", $res));
                     exit();
-                }else{
-                    PostCheck::formatPrintLn(['green'],'done');
+                } else {
+                    PostCheck::formatPrintLn(['green'], 'done');
                 }
-                
+
                 /*
                 if (( $args->getOpt('createusers'))===true){
 
@@ -185,21 +193,23 @@ class CreateSystemCommandline implements ICommandline{
                     
                 }
                 */
-                
-                $sql = "INSERT IGNORE INTO macc_clients (id,username,password,host,port) VALUES ('".$clientDBName."','".App::configuration('','__SESSION_USER__','localhost')."','','".App::configuration('','__SESSION_HOST__','localhost')."',".App::configuration('','__SESSION_PORT__','localhost').")";
-                exec('echo "'.$sql.'" | mysql '.$clientOptions.' --force=true -D '.$sessionDBName.' ',$res,$err);
-                
-                PostCheck::formatPrint(['blue'],"\tcreate client user... admin: ".$clientpassword." \n");
-                $sql = "call ADD_TUALO_USER('".$clientUsername."','".$clientpassword."','".$clientDBName."','administration')";
-                exec('echo "'.$sql.'" | mysql '.$clientOptions.' --force=true -D '.$sessionDBName.' ',$res,$err);
+
+                $sql = "INSERT IGNORE INTO macc_clients (id,username,password,host,port) VALUES ('" . $clientDBName . "','" .
+                    App::configuration('database', 'db_user', 'localhost') . "','','" .
+                    App::configuration('database', 'db_host', 'localhost') . "'," .
+                    App::configuration('database', 'db_port', '3306') . ")";
+                exec('echo "' . $sql . '" | mysql ' . $clientOptions . ' --force=true -D ' . $sessionDBName . ' ', $res, $err);
+
+                PostCheck::formatPrint(['blue'], "\tcreate client user... admin: " . $clientpassword . " \n");
+                $sql = "call ADD_TUALO_USER('" . $clientUsername . "','" . $clientpassword . "','" . $clientDBName . "','administration')";
+                exec('echo "' . $sql . '" | mysql ' . $clientOptions . ' --force=true -D ' . $sessionDBName . ' ', $res, $err);
 
 
                 break;
             }
-            if ($line=='n'){
+            if ($line == 'n') {
                 break;
             }
         }
-
     }
 }
