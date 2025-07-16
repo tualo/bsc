d0dad1d9b2370125583e594bb2f1c44ab7facece
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-07-16 13:52:42 +0200

    fix class namespace

--- src/Routes/PublicRoute.php
+++ src/Routes/PublicRoute.php
@@ -1,35 +1,43 @@
 <?php
-namespace Tualo\Office\CMS\Routes;
+
+namespace Tualo\Office\BSC\Routes;
+
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
 
 
-class PublicRoute implements IRoute{
-    
-    public static function register(){
-        BasicRoute::add('/public/(?P<path>.*)',function($matches){
-            
-                
+class PublicRoute implements IRoute
+{
+
+    public static function register()
+    {
+        BasicRoute::add('/public/(?P<path>.*)', function ($matches) {
+
+
             $publicpath =  App::configuration(
                 'tualo-backend',
                 'public_path'
             );
-            if ($publicpath!==false){
+            if ($publicpath !== false) {
                 if (file_exists(
-                    str_replace('//','/',implode('/',[
+                    str_replace(
+                        '//',
+                        '/',
+                        implode('/', [
+                            $publicpath,
+                            $matches['path']
+                        ])
+                    )
+                )) {
+                    App::etagFile(str_replace('//', '/', implode('/', [
                         $publicpath,
                         $matches['path']
-                    ])
-                ))){
-                    App::etagFile( str_replace('//','/',implode('/',[
-                        $publicpath,
-                        $matches['path']
-                    ])) , true);
+                    ])), true);
                     BasicRoute::$finished = true;
                     http_response_code(200);
                 }
             }
-        },['get','post'],true);
+        }, ['get', 'post'], true);
     }
 }
