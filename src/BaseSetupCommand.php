53cd12db7db4774a8a906837ef95082534311acc
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-07-30 13:53:58 +0200

    simplyfy setup

--- src/BaseSetupCommand.php
+++ src/BaseSetupCommand.php
@@ -1,23 +1,75 @@
 <?php
+
 namespace Tualo\Office\Basic;
+
 use Garden\Cli\Cli;
+use Garden\Cli\Args;
 
-class BaseSetupCommand {
+class BaseSetupCommand implements ISetupCommandline
+{
+    public static function getCommandName(): string
+    {
+        return 'base-setup';
+    }
+    public static function getCommandDescription(): string
+    {
+        return 'perform a base setup';
+    }
+    public static function setup(Cli $cli)
+    {
+        $cli->command(self::getCommandName())
+            ->description(self::getCommandDescription())
+            ->opt('client', 'only use this client', true, 'string');
+    }
 
-    public static function performInstall(string $cmdString,string $clientName) {
-        $cmd = explode(' ',$cmdString);
-        if ($clientName!='') $cmd[] = '--client='.$clientName;
+    // This method should be overridden in child classes to provide specific commands
+    public static function getCommands(Args $args): array
+    {
+        return [
+            'install-sessionsql-bsc-main',
+            'install-sql-sessionviews',
+            'install-sql-bsc-main-ds',
+            'install-sql-bsc-menu',
+
+        ];
+    }
+    public static function performInstall(
+        string $cmdString,
+        string $clientName
+    ) {
+        $cmd = explode(' ', $cmdString);
+        if ($clientName != '') $cmd[] = '--client=' . $clientName;
         $classes = get_declared_classes();
-        foreach($classes as $cls){
+        foreach ($classes as $cls) {
             $class = new \ReflectionClass($cls);
-            if ( $class->implementsInterface('Tualo\Office\Basic\ICommandline') ) {
-                if($cmd[0]==$cls::getCommandName()){
+            if ($class->implementsInterface('Tualo\Office\Basic\ICommandline')) {
+                if ($cmd[0] == $cls::getCommandName()) {
                     $cli = new Cli();
                     $cls::setup($cli);
-                    $args = $cli->parse(['./tm',...$cmd], true);
+                    $args = $cli->parse(['./tm', ...$cmd], true);
                     $cls::run($args);
                 }
             }
+        }
+    }
+
+    public static function getHeadLine(): string
+    {
+        return 'Base Setup Command';
+    }
+
+    public static function run(Args $args)
+    {
+        $clientName = $args->getOpt('client');
+        if (is_null($clientName)) $clientName = '';
+
+        PostCheck::formatPrintLn(['blue'], "Installing all for: " . static::getHeadLine());
+        PostCheck::formatPrintLn(['blue'], "==========================================================");
+
+        $installCommands = static::getCommands($args);
+
+        foreach ($installCommands as $cmdString) {
+            static::performInstall($cmdString, $clientName);
         }
     }
 }
--- src/ISetupCommandline.php
+++ src/ISetupCommandline.php
@@ -1,11 +1,15 @@
 <?php
+
 namespace Tualo\Office\Basic;
+
 use Garden\Cli\Cli;
 use Garden\Cli\Args;
 
 interface ISetupCommandline
 {
-    public static function getCommandName():string;
-    public static function getCommandDescription():string;
+    public static function getHeadLine(): string;
+    public static function getCommands(Args $args): array;
+    public static function getCommandName(): string;
+    public static function getCommandDescription(): string;
     public static function run(Args $args);
 }
